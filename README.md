dd3c1f292ee6da79a900d06b3d3a9b2f6f8b9155
u-yas <yuya3.21998@gmail.com>
2022-12-04 13:27:22 +0900

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Template FULL STACK WEB APP USING CLOUDFLARE
+
+## Setup
+
+1. yarn install
+2. wrangler login
+3. cp workers/.env.example workers/.env
+4. cp pages/.env.example pages/.env
+5. edit .env and workers/wrangler.toml
+6. cd pages && yarn wrangler pages project create ${YOUR_PAGES_PROJECT}
+
+Use devcontainer as you like.
---
+++ workers/src/index.ts
@@ -0,0 +1,38 @@
+import { Router } from "itty-router";
+const router = Router();
+
+const path = (path: string) => "/api/v1" + path;
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+router.get(path("/health"), async (req) => {
+  return new Response("OK", { status: 200 });
+});
+
+/*
+This is the last route we define, it will match anything that hasn't hit a route we've defined
+above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
+Visit any page that doesn't exist (e.g. /foobar) to see it in action.
+*/
+router.all("*", () => new Response("404, not found!", { status: 404 }));
+
+addEventListener("fetch", (e) => {
+  e.respondWith(router.handle(e.request));
+});
+// export default {
+// 	async fetch(
+// 		request: Request,
+// 		env: Env,
+// 		ctx: ExecutionContext
+// 	): Promise<Response> {
+// 		return new Response("Hello World!");
+// 	},
+// };
